9991a11ad098def5db378b050f3a46d4a27119b4
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-06-07 20:07:07 +0530

    revised chnages

--- stories.js
+++ stories.js
@@ -1,4 +1,12 @@
-import { firebaseConfig } from "./imp.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyB4kvaHLnaGIEPq-NQIuPeSHjyJyyypY7c",
+  authDomain: "queerstation.firebaseapp.com",
+  projectId: "queerstation",
+  storageBucket: "queerstation.appspot.com",
+  messagingSenderId: "1066174228299",
+  appId: "1:1066174228299:web:93cd053bbc2b01027feace",
+  measurementId: "G-1YSZYKSFT6"
+};
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
