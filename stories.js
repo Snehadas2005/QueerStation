dea0ed3cf9377c563ddb8a82f0b13d576bb4d74f
Sneha Das <154408198+Snehadas2005@users.noreply.github.com>
2025-06-07 19:09:17 +0530

    changes made

--- stories.js
+++ stories.js
@@ -1,12 +1,4 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyB4kvaHLnaGIEPq-NQIuPeSHjyJyyypY7c",
-  authDomain: "queerstation.firebaseapp.com",
-  projectId: "queerstation",
-  storageBucket: "queerstation.appspot.com",
-  messagingSenderId: "1066174228299",
-  appId: "1:1066174228299:web:93cd053bbc2b01027feace",
-  measurementId: "G-1YSZYKSFT6"
-};
+import { firebaseConfig } from "./topsecret.js";
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
